009683270e1d7cad71fdaed1e27e7548bc629757
Ioram Gordadze <Yoh0xFF@gmail.com>
2017-04-10 22:42:07 +0400

    tiny fixes

--- src/main/java/util/AutoPrinter.java
+++ src/main/java/util/AutoPrinter.java
@@ -14,13 +14,14 @@ import static java.nio.file.StandardWatchEventKinds.OVERFLOW;
 import java.nio.file.WatchEvent;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Properties;
 import java.util.regex.Pattern;
 import javax.print.PrintService;
 import javax.print.PrintServiceLookup;
 import javax.print.attribute.HashPrintRequestAttributeSet;
 import javax.print.attribute.PrintRequestAttributeSet;
-import javax.print.attribute.standard.Copies;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.pdfbox.pdmodel.PDDocument;
@@ -42,7 +43,7 @@ public class AutoPrinter {
                 : AutoPrinter.class.getResourceAsStream("/config.properties")) {
             props.load(in);
         } catch (IOException | NullPointerException ex) {
-            LOGGER.error("Application config not found!", ex);
+            LOGGER.error("Application configuration not found!", ex);
             throw new AssertionError("Application configuration not found", ex);
         }
 
@@ -54,6 +55,7 @@ public class AutoPrinter {
     private final Path dir;
     private final WatchService watcher;
     private final Pattern fileNamePattern;
+    private final List<Path> trash;
 
     public AutoPrinter() throws IOException {
         watcher = FileSystems.getDefault().newWatchService();
@@ -62,6 +64,8 @@ public class AutoPrinter {
         dir.register(watcher, ENTRY_CREATE);
         
         fileNamePattern = Pattern.compile(FILE_NAME_PATTERN);
+        
+        trash = new LinkedList<>();
     }
 
     public void processEvents() {
@@ -72,6 +76,11 @@ public class AutoPrinter {
             } catch (InterruptedException ex) {
                 LOGGER.error("Watcher interrupted!", ex);
                 return;
+            }
+            
+            while (!trash.isEmpty()) {
+                trash.get(0).toFile().delete();
+                trash.remove(0);
             }
 
             for (WatchEvent<?> event : crntWatchKey.pollEvents()) {
@@ -84,10 +93,11 @@ public class AutoPrinter {
                 try {
                     String mimeType = Files.probeContentType(path);
 
-                    LOGGER.info(String.format("%s, %s, %s\n", event.kind().name(), path, mimeType));
+                    LOGGER.info("{}, {}, {}\n", event.kind().name(), path, mimeType);
                     
                     if (FILE_MIME_TYPE.equals(mimeType) && fileNamePattern.matcher(path.toFile().getName()).matches()) {
                         print(path);
+                        trash.add(path);
                     }
                 } catch (IOException ex) {
                     LOGGER.error(String.format("Cannot read file: %s!", path.toString()), ex);
@@ -126,9 +136,10 @@ public class AutoPrinter {
             printerJob.setPrintService(defaultPrintService);
             
             PrintRequestAttributeSet attributes = new HashPrintRequestAttributeSet();
-            attributes.add(new Copies(n));
             
-            printerJob.print(attributes);
+            for (int i = 0; i < n; ++i) {
+                printerJob.print(attributes);
+            }
             LOGGER.info("Printing file: {}!", path);
         } catch (IOException ex) {
             LOGGER.error(String.format("File real failed: %s!", path.toString()), ex);
