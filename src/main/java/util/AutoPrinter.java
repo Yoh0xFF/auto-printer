9c5e27710232fcf56fc98572f93b8ce058ef69c4
Ioram Gordadze <Yoh0xFF@gmail.com>
2021-10-12 22:00:36 +0400

    Upgrade libraries.

--- src/main/java/util/AutoPrinter.java
+++ src/main/java/util/AutoPrinter.java
@@ -32,183 +32,183 @@ import static java.nio.file.StandardWatchEventKinds.OVERFLOW;
 
 public class AutoPrinter {
 
-    private static final Logger logger;
-    private static final String PRINTER_NAME;
-    private static final String WATCH_DIR;
-    private static final String FILE_NAME_PATTERN;
+  private static final Logger logger;
+  private static final String PRINTER_NAME;
+  private static final String WATCH_DIR;
+  private static final String FILE_NAME_PATTERN;
 
-    static {
-        logger = LogManager.getLogger(AutoPrinter.class.getName());
+  static {
+    logger = LogManager.getLogger(AutoPrinter.class.getName());
 
-        Properties props = new Properties();
-        try (InputStream in = Files.exists(Paths.get("./config.properties"))
-                ? new FileInputStream("./config.properties")
-                : AutoPrinter.class.getResourceAsStream("/config.properties")) {
-            props.load(in);
-        } catch (IOException | NullPointerException ex) {
-            logger.error("Application configuration not found!\n", ex);
-            throw new AssertionError("Application configuration not found", ex);
-        }
-
-        PRINTER_NAME = props.getProperty("printerName");
-        WATCH_DIR = props.getProperty("watchDir");
-        FILE_NAME_PATTERN = props.getProperty("fileNamePattern");
+    Properties props = new Properties();
+    try (InputStream in = Files.exists(Paths.get("./config.properties"))
+      ? new FileInputStream("./config.properties")
+      : AutoPrinter.class.getResourceAsStream("/config.properties")) {
+      props.load(in);
+    } catch (IOException | NullPointerException ex) {
+      logger.error("Application configuration not found!\n", ex);
+      throw new AssertionError("Application configuration not found", ex);
     }
 
-    private static AutoPrinter instance;
-    private final Path dir;
-    private final WatchService watcher;
-    private final Pattern fileNamePattern;
-    private final List<Path> trash;
+    PRINTER_NAME = props.getProperty("printerName");
+    WATCH_DIR = props.getProperty("watchDir");
+    FILE_NAME_PATTERN = props.getProperty("fileNamePattern");
+  }
 
-    private AutoPrinter() throws IOException {
-        watcher = FileSystems.getDefault().newWatchService();
+  private static AutoPrinter instance;
+  private final Path dir;
+  private final WatchService watcher;
+  private final Pattern fileNamePattern;
+  private final List<Path> trash;
 
-        dir = Paths.get(WATCH_DIR);
-        dir.register(watcher, ENTRY_CREATE);
+  private AutoPrinter() throws IOException {
+    watcher = FileSystems.getDefault().newWatchService();
 
-        fileNamePattern = Pattern.compile(FILE_NAME_PATTERN);
+    dir = Paths.get(WATCH_DIR);
+    dir.register(watcher, ENTRY_CREATE);
 
-        trash = new LinkedList<>();
+    fileNamePattern = Pattern.compile(FILE_NAME_PATTERN);
+
+    trash = new LinkedList<>();
+  }
+
+  private void processEvents() {
+    while (true) {
+      WatchKey crntWatchKey;
+      try {
+        crntWatchKey = watcher.take();
+      } catch (InterruptedException ex) {
+        logger.error("Watcher interrupted!\n", ex);
+        break;
+      } catch (ClosedWatchServiceException ex) {
+        logger.info("Watcher is closed!\n");
+        break;
+      }
+
+      while (!trash.isEmpty()) {
+        if (trash.get(0).toFile().exists()) {
+          trash.get(0).toFile().delete();
+        }
+        trash.remove(0);
+      }
+
+      for (WatchEvent<?> event : crntWatchKey.pollEvents()) {
+        if (event.kind() == OVERFLOW) {
+          continue;
+        }
+
+        Path path = dir.resolve(((WatchEvent<Path>) event).context());
+
+        logger.info("{}, {}\n", event.kind().name(), path);
+
+        if (fileNamePattern.matcher(path.toFile().getName()).matches()) {
+          print(path);
+        }
+      }
+
+      boolean valid = crntWatchKey.reset();
+      if (!valid) {
+        logger.error("WatchKey reset failed!\n");
+        break;
+      }
+    }
+  }
+
+  private void print(Path path) {
+    PrintService[] printServices = PrintServiceLookup.lookupPrintServices(null, null);
+
+    PrintService defaultPrintService = null;
+    for (PrintService printService : printServices) {
+      if (printService.getName().equals(PRINTER_NAME)) {
+        defaultPrintService = printService;
+        break;
+      }
     }
 
-    private void processEvents() {
-        while (true) {
-            WatchKey crntWatchKey;
-            try {
-                crntWatchKey = watcher.take();
-            } catch (InterruptedException ex) {
-                logger.error("Watcher interrupted!\n", ex);
-                break;
-            } catch (ClosedWatchServiceException ex) {
-                logger.info("Watcher is closed!\n");
-                break;
-            }
-
-            while (!trash.isEmpty()) {
-                if (trash.get(0).toFile().exists()) {
-                    trash.get(0).toFile().delete();
-                }
-                trash.remove(0);
-            }
-
-            for (WatchEvent<?> event : crntWatchKey.pollEvents()) {
-                if (event.kind() == OVERFLOW) {
-                    continue;
-                }
-
-                Path path = dir.resolve(((WatchEvent<Path>) event).context());
-
-                logger.info("{}, {}\n", event.kind().name(), path);
-
-                if (fileNamePattern.matcher(path.toFile().getName()).matches()) {
-                    print(path);
-                }
-            }
-
-            boolean valid = crntWatchKey.reset();
-            if (!valid) {
-                logger.error("WatchKey reset failed!\n");
-                break;
-            }
-        }
+    if (defaultPrintService == null) {
+      logger.error("Printer with specified name: {}, not found!\n", PRINTER_NAME);
+      return;
     }
 
-    private void print(Path path) {
-        PrintService[] printServices = PrintServiceLookup.lookupPrintServices(null, null);
+    // After download some browsers scan file for viruses and because of this file is locked
+    boolean locked = true;
+    while (locked) {
+      try (FileInputStream is = new FileInputStream(path.toFile())) {
+        locked = false;
+      } catch (IOException ex) {
+        locked = ex.getMessage().contains("used by another process");
+      }
 
-        PrintService defaultPrintService = null;
-        for (PrintService printService : printServices) {
-            if (printService.getName().equals(PRINTER_NAME)) {
-                defaultPrintService = printService;
-                break;
-            }
-        }
-
-        if (defaultPrintService == null) {
-            logger.error("Printer with specified name: {}, not found!\n", PRINTER_NAME);
-            return;
-        }
-
-        // After download some browsers scan file for viruses and because of this file is locked
-        boolean locked = true;
-        while (locked) {
-            try (FileInputStream is = new FileInputStream(path.toFile())) {
-                locked = false;
-            } catch (IOException ex) {
-                locked = ex.getMessage().contains("used by another process");
-            }
-
-            if (locked) {
-                try {
-                    Thread.sleep(1000L);
-                } catch (InterruptedException ex) {
-                }
-            }
-        }
-
-        // Old browsers create temporary files while downloading and then delete it
-        if (!path.toFile().exists()) {
-            return;
-        }
-
-        int n = path.toFile().getName().toLowerCase().contains("thermal") ? 2 : 1;
-        int cnt = 3;
-        
-        // Some browsers doesn't create temorary files, and we have wait for download to finish.
-        // We are waiting only 3 seconds
-        while (cnt > 0) {
-            try (PDDocument pdfDoc = PDDocument.load(path.toFile())) {
-                PrinterJob printerJob = PrinterJob.getPrinterJob();
-                printerJob.setPageable(new PDFPageable(pdfDoc));
-                printerJob.setPrintService(defaultPrintService);
-
-                PrintRequestAttributeSet attributes = new HashPrintRequestAttributeSet();
-
-                for (int i = 0; i < n; ++i) {
-                    printerJob.print(attributes);
-                }
-                
-                logger.info("Printing file: {}!\n", path);
-                
-                cnt = 0;
-            } catch (IOException ex) {
-                try {
-                    Thread.sleep(1000L);
-                } catch (InterruptedException ex1) {
-                }
-                
-                --cnt;
-                
-                if (cnt == 0) {
-                    logger.error(String.format("File read failed: %s!\n", path.toString()), ex);
-                }
-            } catch (PrinterException ex) {
-                logger.error(String.format("File print failed: %s!\n", path.toString()), ex);
-            }
-        }
-
-        trash.add(path);
-    }
-
-    public static void stop(String[] args) {
+      if (locked) {
         try {
-            instance.watcher.close();
-        } catch (IOException ex) {
-            logger.error("Watcher close failed!\n", ex);
+          Thread.sleep(1000L);
+        } catch (InterruptedException ex) {
         }
+      }
     }
 
-    public static void start(String[] args) {
+    // Old browsers create temporary files while downloading and then delete it
+    if (!path.toFile().exists()) {
+      return;
+    }
+
+    int n = path.toFile().getName().toLowerCase().contains("thermal") ? 2 : 1;
+    int cnt = 3;
+
+    // Some browsers doesn't create temorary files, and we have wait for download to finish.
+    // We are waiting only 3 seconds
+    while (cnt > 0) {
+      try (PDDocument pdfDoc = PDDocument.load(path.toFile())) {
+        PrinterJob printerJob = PrinterJob.getPrinterJob();
+        printerJob.setPageable(new PDFPageable(pdfDoc));
+        printerJob.setPrintService(defaultPrintService);
+
+        PrintRequestAttributeSet attributes = new HashPrintRequestAttributeSet();
+
+        for (int i = 0; i < n; ++i) {
+          printerJob.print(attributes);
+        }
+
+        logger.info("Printing file: {}!\n", path);
+
+        cnt = 0;
+      } catch (IOException ex) {
         try {
-            instance = new AutoPrinter();
-            instance.processEvents();
-        } catch (Exception ex) {
-            logger.error("Watcher startup failed!\n", ex);
+          Thread.sleep(1000L);
+        } catch (InterruptedException ex1) {
         }
+
+        --cnt;
+
+        if (cnt == 0) {
+          logger.error(String.format("File read failed: %s!\n", path.toString()), ex);
+        }
+      } catch (PrinterException ex) {
+        logger.error(String.format("File print failed: %s!\n", path.toString()), ex);
+      }
     }
 
-    public static void main(String[] args) {
-        start(args);
+    trash.add(path);
+  }
+
+  public static void stop(String[] args) {
+    try {
+      instance.watcher.close();
+    } catch (IOException ex) {
+      logger.error("Watcher close failed!\n", ex);
     }
+  }
+
+  public static void start(String[] args) {
+    try {
+      instance = new AutoPrinter();
+      instance.processEvents();
+    } catch (Exception ex) {
+      logger.error("Watcher startup failed!\n", ex);
+    }
+  }
+
+  public static void main(String[] args) {
+    start(args);
+  }
 }
